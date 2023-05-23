47100dc190990870e89bd3b7179640f78f03da54
Yaroslav <set.oplata@gmail.com>
2023-05-23 17:59:40 +0200

    1. Introduced method 'update' into class ArrayStorage. 2. Added packages

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -6,51 +6,10 @@ import java.util.Arrays;
 
 public class ArrayStorage {
     private static final int CAPACITY = 10000;
+    private static final String NOT_FOUND = " - entered resume was not found";
     Resume[] storage = new Resume[CAPACITY];
     private int countResumes;
-
-    public void clear() {
-        Arrays.fill(storage, 0, countResumes, null);
-        countResumes = 0;
-    }
-
-    public void save(Resume resume) {
-        if (countResumes == CAPACITY) {
-            System.out.println("The resume storage is full");
-            return;
-        }
-
-        for (int i = 0; i < countResumes; i++) {
-            if (resume.toString().equals(storage[i].getUuid())) {
-                System.out.println("The resume '" + resume + "' already added.");
-                return;
-            }
-        }
-        storage[countResumes] = resume;
-        countResumes++;
-    }
-
-    public Resume get(String uuid) {
-        for (int i = 0; i < countResumes; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                return storage[i];
-            }
-        }
-        return null;
-    }
-
-    public void delete(String uuid) {
-        for (int i = 0; i < countResumes; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                countResumes--;
-                System.arraycopy(storage, i + 1, storage, i, countResumes - i);
-                storage[countResumes] = null;
-                System.out.println("The resume '" + uuid + "' was successfully deleted.");
-                return;
-            }
-        }
-        System.out.println("The entered resume '" + uuid + "' was not found for deletion.");
-    }
+    private int numberElement;
 
     public Resume[] getAll() {
         return Arrays.copyOf(storage, countResumes);
@@ -60,13 +19,60 @@ public class ArrayStorage {
         return countResumes;
     }
 
+    public void save(Resume resume) {
+        if (countResumes == CAPACITY) {
+            System.out.println("The resume storage is full");
+            return;
+        }
+
+        if (checkUuid(resume.toString())) {
+            System.out.println("Such resume '" + resume + "' already added.");
+            return;
+        }
+        storage[countResumes] = resume;
+        countResumes++;
+    }
+
+    public void delete(String uuid) {
+        if (checkUuid(uuid)) {
+            countResumes--;
+            System.arraycopy(storage, numberElement + 1, storage,
+                    numberElement, countResumes - numberElement);
+            storage[countResumes] = null;
+            System.out.println("The resume '" + uuid + "' was successfully deleted.");
+            return;
+        }
+        System.out.println(uuid + NOT_FOUND);
+    }
+
+    public Resume get(String uuid) {
+        if (checkUuid(uuid)) {
+            return storage[numberElement];
+        }
+        System.out.println(uuid + NOT_FOUND);
+        return null;
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, countResumes, null);
+        countResumes = 0;
+    }
+
     public void update(Resume resume, Resume newResume) {
+        if (checkUuid(resume.toString())) {
+            storage[numberElement].setUuid(newResume.toString());
+            return;
+        }
+        System.out.println(resume + NOT_FOUND);
+    }
+
+    public boolean checkUuid(String uuid) {
         for (int i = 0; i < countResumes; i++) {
-            if (resume.equals(storage[i])) {
-                storage[i].setUuid(newResume.toString());
-                return;
+            if (uuid.equals(storage[i].getUuid())) {
+                numberElement = i;
+                return true;
             }
         }
-        System.out.println("Entered resume: '" + resume + "' was not found for update.");
+        return false;
     }
 }
