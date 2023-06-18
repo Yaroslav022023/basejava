56a83e363e55f9edb00bc712088a2057b0a775a6
Yaroslav <set.oplata@gmail.com>
2023-06-18 14:43:52 +0200

    Corrected test classes.

--- test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -3,6 +3,7 @@ package com.basejava.webapp.storage;
 import com.basejava.webapp.exceptions.*;
 import com.basejava.webapp.model.Resume;
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class AbstractArrayStorageTest {
