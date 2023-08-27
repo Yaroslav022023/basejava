92f6977aa4c002cd622012193234ca4ae2cf67f7
Yaroslav <set.oplata@gmail.com>
2023-08-27 19:11:04 +0200

    1. Refactored 'DeadLock.class' (took out logic into a separate method). 2. Introduced Logger.

--- src/com/basejava/webapp/DeadLock.java
+++ src/com/basejava/webapp/DeadLock.java
@@ -1,24 +1,32 @@
 package com.basejava.webapp;
 
+import java.util.logging.Logger;
+
 public class DeadLock {
+    private static final Logger LOG = Logger.getLogger(DeadLock.class.getName());
     private static final Object LOCK_1 = new Object();
     private static final Object LOCK_2 = new Object();
 
     public static void main(String[] args) {
-        new Thread(() -> actThread(LOCK_1, LOCK_2)).start();
-        new Thread(() -> actThread(LOCK_2, LOCK_1)).start();
+        actThread(LOCK_1, LOCK_2);
+        actThread(LOCK_2, LOCK_1);
     }
 
     private static void actThread(Object lock1, Object lock2) {
-        synchronized (lock1) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        new Thread(() -> {
+            LOG.info("Trying to lock Object 1 - " + Thread.currentThread().getName());
+            synchronized (lock1) {
+                LOG.info("Locked Object 1 - " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                LOG.info("Trying to lock Object 2 - " + Thread.currentThread().getName());
+                synchronized (lock2) {
+                    LOG.info("Locked Object 2 - " + Thread.currentThread().getName());
+                }
             }
-            synchronized (lock2) {
-                System.out.println("Succeeded to capture the monitor LOCK_2");
-            }
-        }
+        }).start();
     }
 }
